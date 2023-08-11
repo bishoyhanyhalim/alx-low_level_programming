f5de303f884f525d645cfb3c396443b987e15393
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-11 16:44:55 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	return (0);
+}
