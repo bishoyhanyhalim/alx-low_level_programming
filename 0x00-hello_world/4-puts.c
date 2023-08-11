e2bb24e0a1474812e6d69c9dc63d2c5714261881
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-10 18:14:32 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+include < stdio.h >
+
+/**
+ * main - print "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 all time
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
