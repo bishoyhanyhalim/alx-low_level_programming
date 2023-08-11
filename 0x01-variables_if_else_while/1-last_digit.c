efaa166d1ead72ef952e48871318bc0e464509b5
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-11 10:57:07 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 		printf("Last digit of %d is %d and is 0", n, last_num);
 	} else if (n < 6 && n != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, lsat_num);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_num);
 	}
 	return (0);
 }
