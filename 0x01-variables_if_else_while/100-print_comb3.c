075c2805a7234a1b25280065915f0cf7ddd45bde
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-11 18:34:39 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int f_num;
+	int s_num;
+
+	for (f_num = 0; f_num <= 8; f_num++)
+	{
+		for (s_num = f_num + 1; s_num <=9; s_num++)
+		{
+			putchar(f_num + '0');
+			putchar(s_num + '0');
+
+			if (f_num != 8 || s_num !=9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
