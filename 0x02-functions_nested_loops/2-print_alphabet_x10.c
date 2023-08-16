c071e91f993df642ba1cf803aabf0257b443f7d4
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-15 18:33:44 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_alphabet_x10 - for print  print_alphabet_x10
+ *
+ * Return:0 always
+ */
+void print_alphabet_x10(void)
+{
+	int ch;
+	int num;
+
+	for (num = 0; num < 10; num++)
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+	_putchar('\n');
+}
