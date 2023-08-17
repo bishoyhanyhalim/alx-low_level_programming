99bcd14c9c96e6c1510f0ae85faf1ba4683e59d0
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-17 10:15:30 -0700

    13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number, total;
+
+	for (number = 0; number < 1024; number++)
+	{
+		if ((number % 3 == 0) || (number % 5 == 0))
+		{
+			total += number;
+		}
+	}
+
+	printf("%d", total);
+	return (0);
+}
