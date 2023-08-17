77196f313e082940b97757f61f279e57bd37e1af
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-17 09:44:57 -0700

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,29 +10,31 @@ void print_times_table(int n)
 
 	if (n <= 15 && n >= 0)
 	{
-		_putchar(48);
-		for (multnum = 1; multnum <= n; multnum++)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(' ');
-
-			prod = num * multnum;
-
-			if (prod <= 9)
-				_putchar(' ');
-			if (prod <= 99)
-				_putchar(' ');
-
-			if (prod >= 100)
+			_putchar(48);
+			for (multnum = 1; multnum <= n; multnum++)
 			{
-				_putchar((prod / 100) + '0');
-				_putchar((prod / 10) % 10 + '0');
-			} else if (prod <= 99 && prod >= 10)
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				prod = num * multnum;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
