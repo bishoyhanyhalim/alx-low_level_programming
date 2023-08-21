ccd158c6c1a17892361d6370fd7fc52863965839
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-21 16:00:10 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int h = 0;
+
+	while (s[h] != '\0')
+	{
+		h++;
+	}
+	return (i);
+}
