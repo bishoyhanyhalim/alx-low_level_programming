84acc32ffe71a5e3364263178e16308634809ee7
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-21 16:48:23 -0700

    11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int i, sum, s;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			s = 2772 - sum - '4';
+			sum += s;
+			putchar(s + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
