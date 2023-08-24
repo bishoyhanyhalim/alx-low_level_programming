b114724a7e5eb896e7adc51d8fbf4dea15810d0d
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-08-24 02:30:52 -0700

    8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in code
+ * @do: string to be encoded
+ *
+ * Return: the resulting string
+ */
+char *rot13(char *do)
+{
+	int num;
+
+	char rot13[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char ROT13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char *ptr = s;
+
+	while (*do)
+	{
+		for (num = 0; num <= 52; num++)
+		{
+			if (*do == rot13[num])
+			{
+				*do = ROT13[num];
+				break;
+			}
+		}
+		do++;
+	}
+	return (ptr);
+}
