1a8e51acf6bf79456ed17ced71efa0164946b444
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-09-02 19:16:57 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the number
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+}
