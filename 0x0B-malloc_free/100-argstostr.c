7a571431e964292b6bb1c96f86fdd7df9bac4ca7
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-09-06 05:46:30 -0700

    5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *argstostr - concatenates prog
+  *@ac: argument count
+  *@av: pointer of array
+  *Return: null code
+  */
+char *argstostr(int ac, char **av)
+{
+	int value = 0;
+	int hello = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (int num = 0; num < ac; num++)
+	{
+		for (int cool = 0; av[num][cool]; cool++)
+		{
+			hello++;
+		}
+		hello++;
+	}
+
+	char *arg = malloc(hello + ac);
+
+	if (arg == NULL)
+	{
+		return (NULL);
+	}
+
+	for (int num = 0; num < ac; num++)
+	{
+		for (int cool = 0; av[num][cool]; cool++)
+		{
+			arg[value++] = av[num][cool];
+		}
+		arg[value++] = '\n';
+	}
+
+	arg[value] = '\0';
+
+	return (arg);
+}
