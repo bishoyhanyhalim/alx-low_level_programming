efc7df799e44480698224de95823942069a5c865
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-09-07 17:26:17 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * *string_nconcat - n bytes to string
@@ -12,25 +11,37 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *go;
+	unsigned int num1;
+	unsigned int num2;
+	unsigned int num3 = 0;
 
-	if (s1 == NULL)
-	{
+
+	if (!s1)
 		s1 = "";
-	}
+	if (!s2)
+		s2 = "";
 
-	size_t total1 = strlen(s1);
-	size_t total2 = strlen(s2);
-	size_t look = (n >= total2) ? (total1 + total2) : (total1 + n);
+	while (s1[num3])
+		num3++;
 
-	go = malloc(look + 1);
+	go = malloc(sizeof(char) * (num3 + n + 1));
 
-	if (go == NULL)
+	if (!go)
 	{
 		return (NULL);
 	}
 
-	strcpy(go, s1);
-	strncat(go, s2, n);
+	for (num1 = 0; *s1; num1++, s1++)
+	{
+		go[num1] = *s1;
+	}
+
+	for (num1 = num3, num2 = 0; num2 < n && s2[num2]; num1++, num2++)
+	{
+		go[num1] = s2[num2];
+	}
+
+	go[num3 + n] = '\0';
 
 	return (go);
 }
