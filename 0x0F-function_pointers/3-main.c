f88e945f9083fb17e27fdfa1f19ffd7e32fa2a34
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-09-13 15:32:34 -0700

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,18 +1,21 @@
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
-  * main - print the sum
-  * @argc: pointer of fun
-  * @argv: pointer of fun
-  *
-  * Return: result of calc
-  */
-
+ * main - return the calc of num
+ * @argc: it is the char
+ * @argv: it is the vector
+ *
+ * Return: return the numbers
+ */
 int main(int argc, char *argv[])
 {
-	int (*luke)(int, int);
+	int number1;
+	int number2;
+	int suming;
+	char look;
+	int (*luky)(int, int);
 
 	if (argc != 4)
 	{
@@ -20,15 +23,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	luke = get_op_func(argv[2]);
+	number1 = atoi(argv[1]);
+	number2 = atoi(argv[3]);
 
-	if (!luke)
+	luky = get_op_func(argv[2]);
+
+	if (!luky)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", luke(atoi(argv[1]), atoi(argv[3])));
+	look = *argv[2];
+
+	if ((look == '/' || look == '%') && number2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	suming = luky(number1, number2);
+
+	printf("%d\n", suming);
 
 	return (0);
 }
