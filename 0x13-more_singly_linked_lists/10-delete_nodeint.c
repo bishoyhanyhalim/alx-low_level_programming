aa8cfc336db3c320a7d72800f84e3a1a48eb6370
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-09-26 17:48:21 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,1 +1,45 @@
-sgthfgjdjgh
+i#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - put the code list
+ * @head: look for the first char int list
+ * @index: this is index value
+ *
+ * Return: return the value of code
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int goby = 0;
+	listint_t *moons = NULL;
+	listint_t *full = *head;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(full);
+		return (1);
+	}
+
+	while (goby < index - 1)
+	{
+		if (!full || !(full->next))
+		{
+			return (-1);
+		}
+		full = full->next;
+		goby++;
+	}
+
+	moons = full->next;
+	full->next = moons->next;
+
+	free(moons);
+
+	return (1);
+}
