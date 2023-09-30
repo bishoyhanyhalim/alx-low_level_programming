e7c56ee5bbbee58a4e49e541e6e45155783f3c22
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-09-29 19:22:15 -0700

    100-

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,14 +3,14 @@
 /**
  * get_endianness - get the num
  *
- * Return: return the num value 
+ * Return: return the num value
  */
 
 int get_endianness(void)
 {
 	int flow = 1;
 
-	if (*(char *) & flow == 1)
+	if (*(char *)&flow == 1)
 	{
 		return (1);
 	}
