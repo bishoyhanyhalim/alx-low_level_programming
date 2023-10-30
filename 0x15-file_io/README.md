d2b076a4df270c2deb9c4a8396638ce4c5c58578
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-10-30 16:36:27 -0700

    get ready files

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+hello task files welcome
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef HELLO_FILES
+#define HELLO_FILES
+
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
