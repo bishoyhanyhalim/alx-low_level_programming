42d2a1f958aa34bb4715667c7aec8c868ea16b08
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2023-12-11 15:19:34 -0800

    task 3 ad

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,63 @@
+/**
+ * mod - this func to model numer
+ * @note: this is first num
+ * @book: this is second num
+ * Return: return value
+ */
+
+int mod(int note, int book)
+{
+	return (note % book);
+}
+
+
+/**
+ * mul - this func to darb num
+ * @book: this is first num
+ * @note: this is second num
+ * Return: return value
+ */
+
+int mul(int book, int note)
+{
+	return (book * note);
+}
+
+
+/**
+ * add - this func to plus num
+ * @go: this is first num
+ * @too: this is second num
+ * Return: return value
+ */
+
+int add(int go, int too)
+{
+	return (go + too);
+}
+
+
+/**
+ * div - this func to david num
+ * @hello: this is first num
+ * @from: this is second num
+ * Return: return value
+ */
+
+int div(int hello, int from)
+{
+	return (hello / from);
+}
+
+
+/**
+ * sub - subtracts two integers
+ * @cool: first integer to add
+ * @look: second integer to add
+ * Return: the subtract
+ */
+
+int sub(int cool, int look)
+{
+	return (cool - look);
+}
