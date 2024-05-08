c91aab77119f59875ad3fcce1c92f1b2d5190b28
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-05-08 15:28:26 -0700

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for array func
+ *
+ * @array: input to array
+ * @size: size the array
+ * @value: value of func
+ * Return: Always the result
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int low, mid, high;
+	int x;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (x = low; x <= high; x++)
+			printf("%i%s", array[x], x == high ? "\n" : ", ");
+
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (mid);
+	}
+
+	return (-1);
+}
